259012e1dbe4f9e3085fa631708c54cb32c017f9
幸福的渐变色 <m13552057527@163.com>
2017-07-09 13:07:39 +0800

    02

--- 01-less.js
+++ 01-less.js
@@ -4,6 +4,7 @@ less.render('.cl; a; s; s  width: (1 + 1 }', function (e, output) {
   // console.log( e );
   // console.log(output.css);
   if ( !e ) {
-    console.log( output.css );
+    console.log( output.css );console.log("你好")
+     
   }
 });
